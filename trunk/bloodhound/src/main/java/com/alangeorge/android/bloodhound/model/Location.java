8016331de1f83e3e05d8a549a91334a0e4910bb7
alanwgeorge <alanwgeorge@1e19a868-32f2-5185-9f5a-61d8ab6dd9d6>
2014-06-07 22:52:07 +0000

    init import

---
+++ trunk/bloodhound/src/main/java/com/alangeorge/android/bloodhound/model/Location.java
@@ -0,0 +1,52 @@
+package com.alangeorge.android.bloodhound.model;
+
+import java.util.Date;
+
+public class Location {
+    private long id;
+    private double latitude;
+    private double longitude;
+    private Date time;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", time=" + time +
+                '}';
+    }
+}
