fc52126b44d4e5f05f29cdaff57c52060faddf64
alan.wesley.george@gmail.com <alan.wesley.george@gmail.com@1e19a868-32f2-5185-9f5a-61d8ab6dd9d6>
2014-10-24 18:24:00 +0000

    odds and ends

--- trunk/bloodhound/src/main/java/com/alangeorge/android/bloodhound/ServiceThreadFactory.java
+++ trunk/bloodhound/src/main/java/com/alangeorge/android/bloodhound/ServiceThreadFactory.java
@@ -1,14 +1,12 @@
 package com.alangeorge.android.bloodhound;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
 class ServiceThreadFactory implements ThreadFactory {
 
     @Override
-    public Thread newThread(@NotNull Runnable runnable) {
+    public Thread newThread(@SuppressWarnings("NullableProblems") Runnable runnable) {
         ThreadFactory wrappedFactory = Executors.defaultThreadFactory();
 
         Thread thread = wrappedFactory.newThread(runnable);
